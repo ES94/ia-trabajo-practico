56f179688a78ffe08d79cf4106aa47d94007433f
ES94 <e.stradella94@outlook.com>
2018-11-01 22:01:56 -0300

    Creación de archivo y de funciones: is_goal, actions, result, cost, heuristic y resolver.

---
+++ entrega1.py
@@ -0,0 +1,27 @@
+def is_goal(estado):
+    pass
+
+
+def actions(estado):
+    pass
+
+
+def result(estado, accion):
+    pass
+
+
+def cost(estado1, accion, estado2):
+    pass
+
+
+def heuristic(estado):
+    pass
+
+
+def resolver(metodo_busqueda, posiciones_personas):
+    """
+    Argumentos:
+    metodo_busqueda -- nombre de método a usar (string);
+    posiciones_personas -- lista de posiciones donde se encuentran personas (tuple<tuple<int>>)
+    """
+    pass
