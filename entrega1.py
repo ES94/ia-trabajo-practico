0c5a12972e268b09110b8c2e6ae6106af47d26fc
ES94 <e.stradella94@outlook.com>
2018-11-08 23:55:00 -0300

    Corrección de código y agregación de estadísticas.

--- entrega1.py
+++ entrega1.py
@@ -14,7 +14,7 @@ for i in range(6):
 for i in range(6):
     ORILLA.append((i,5))
 
-# INICIAL = ((0,0) , [] , posiciones_Personas , False)
+# INICIAL = ((0,0) , () , posiciones_Personas , False)
 
 class RescatePersonas(SearchProblem):
     def is_goal(self, estado):
@@ -46,99 +46,43 @@ class RescatePersonas(SearchProblem):
     def result(self, estado, accion):
         pos_x = estado[0][0] + accion[0]
         pos_y = estado[0][1] + accion[1]
-        pisados = estado[1]
-        pos_personas = estado[2]
+        pisados = list(estado[1])
+        pos_personas = list(estado[2])
         ocupado = estado[3]
-        for persona in range(0, len(pos_personas)):  # Rescate de personas. 
-                                                     # Cuando pisa la orilla 
-                                                     # el robot se 'desocupa',
-                                                     # si estaba cargando a 
-                                                     # una persona.
-            if ((pos_x,pos_y) == pos_personas[persona]):
-                pos_personas.pop(persona)
-                if not(ocupado):
-                    ocupado = True
-        if ((pos_x,pos_y) in ORILLA):
-            if (ocupado):
+
+        if (pos_x, pos_y) in pos_personas:     # Rescate de personas. Cuando
+            pos_personas.remove((pos_x, pos_y))  # pisa la orilla el robot se 
+            if not ocupado:                    # 'desocupa', si estaba 
+                ocupado = True                 # cargando a una persona.
+
+        if (pos_x,pos_y) in ORILLA:
+            if ocupado:
                 ocupado = False
         if ((pos_x,pos_y) not in ORILLA):  # Si el robot se mueve a un 
-                                           # casillero 'hundible', lo agrego a
+            pisados.append((pos_x,pos_y))  # casillero 'hundible', lo agrego a
                                            # pisados.
-            pisados.append((pos_x,pos_y))    
-        
-        return ([(pos_x, pos_y), pisados, pos_personas, ocupado])
+        return ((pos_x, pos_y), tuple(pisados), tuple(pos_personas), ocupado)
 
     def cost(self, estado1, accion, estado2):
         return 1
 
-    """
-    def heuristic(self, estado):
-        costo_heuristica = 0
-        pos_robot = list(estado[0])
-        pos_personas = tuple(estado[2])
-
-        # Sumarizar las distancias Manhattan de las personas.
-        for pos_per in pos_personas:
-            costo_heuristica += (abs(pos_per[0] - pos_robot[0]) + 
-                                abs(pos_per[1] - pos_robot[1]))
-            pos_robot = [pos_per[0], pos_per[1]]
-        costo_heuristica += min(pos_robot[0], pos_robot[1], 5 - pos_robot[0], 
-                                5 - pos_robot[1])  # Distancia mínima a una 
-                                                   # orilla.
-        return costo_heuristica
-    """
-
-    """
-    def heuristic(self, estado):
-        costo_heuristica = 0
-        pos_personas = tuple(estado[2])
-
-        # Sumarizar las distancias Manhattan de las personas.
-        for pos_per in pos_personas:
-            costo_heuristica += max(pos_per[0], pos_per[1], 
-                                5 - pos_per[0], 5 - pos_per[1])
-        return costo_heuristica
-    """
-    
-    
-    """
     def heuristic(self, estado):  #Sumatoria de distancia de robot a persona y de esa persona a la orilla mas cercana.
         costos_heuristica = []
         pos_personas = tuple(estado[2])
         pos_robot = list(estado[0])
 
-        for pos_per in pos_personas:
-            distanciaX = abs(pos_robot[0] - pos_per[0])
-            distanciaY = abs(pos_robot[1] - pos_per[1])
+        if pos_personas:
+            for pos_per in pos_personas:
+                distanciaX = abs(pos_robot[0] - pos_per[0])
+                distanciaY = abs(pos_robot[1] - pos_per[1]) 
+                distanciaXY = distanciaX + distanciaY
+                mas_cercanas = [pos_per[0], pos_per[1], 5 - pos_per[0], 
+                                    5 - pos_per[1]]
+                costos_heuristica.append(min(mas_cercanas) + distanciaXY)  #guardo el costo de distancia desde la persona a la orilla mas cercana + la distancia del robot a la persona
             
-            distanciaXY = distanciaX + distanciaY
-            mas_cercanas = []
-            
-            for i in ORILLA:
-                pos_orilla_x = i[0]
-                pos_orilla_y = i[1]
-                pos_orilla = pos_orilla_x + pos_orilla_y
-                mas_cercanas.append(abs(distanciaXY - pos_orilla))
-
-            costos_heuristica.append(min(mas_cercanas) + distanciaXY)  #guardo el costo de distancia desde la persona a la orilla mas cercana + la distancia del robot a la persona
-
-        return (max(costos_heuristica))
-    """     
-
-    def heuristic(self, estado):  #Sumatoria de distancia de robot a persona y de esa persona a la orilla mas cercana.
-        costos_heuristica = []
-        pos_personas = tuple(estado[2])
-        pos_robot = list(estado[0])
-
-        for pos_per in pos_personas:
-            distanciaX = abs(pos_robot[0] - pos_per[0])
-            distanciaY = abs(pos_robot[1] - pos_per[1]) 
-            distanciaXY = distanciaX + distanciaY
-            mas_cercanas = [pos_per[0], pos_per[1], 5 - pos_per[0], 
-                                5 - pos_per[1]]
-            costos_heuristica.append(min(mas_cercanas) + distanciaXY)  #guardo el costo de distancia desde la persona a la orilla mas cercana + la distancia del robot a la persona
-
-        return (max(costos_heuristica))
+            return max(costos_heuristica)
+        else:
+            return 0
 
 
 def resolver(metodo_busqueda, posiciones_personas):
@@ -161,28 +105,25 @@ def resolver(metodo_busqueda, posiciones_personas):
     4ta posicion: Booleano. Es True si el robot esta 'ocupado' (esta cargando 
     personas), caso contrario False.
     """
-    visor = BaseViewer()
     ESTADO_INICIAL = ((0,0) , (), posiciones_personas , False)
     problema = RescatePersonas(ESTADO_INICIAL)
 
     if metodo_busqueda == 'astar':
-        result = astar(problema, graph_search=True, viewer=visor)
-    
-    if metodo_busqueda == 'depth_first':
-        result = depth_first(problema, graph_search=True, viewer=visor)
-    
-    if metodo_busqueda == 'breadth_first':
-        result = breadth_first(problema, graph_search=True, viewer=visor)
-
-    if metodo_busqueda == 'greedy':
-        result = greedy(problema, graph_search=True, viewer=visor)
+        return astar(problema, graph_search=True)
+    elif metodo_busqueda == 'depth_first':
+        return depth_first(problema, graph_search=True)
+    elif metodo_busqueda == 'breadth_first':
+        return breadth_first(problema, graph_search=True)
+    elif metodo_busqueda == 'greedy':
+        return greedy(problema, graph_search=True)
 
 
 if __name__ == "__main__":
     my_viewer = BaseViewer()
     ESTADO_INICIAL = ((0,0) , (), ((2, 1), (3, 4), (4, 2)) , False)
     problema = RescatePersonas(ESTADO_INICIAL)
-    result = breadth_first(problema, graph_search=True, viewer=my_viewer)
+    
+    result = astar(problema, graph_search=True, viewer=my_viewer)
     print("Stats: ",my_viewer.stats)
     print("Solucion: ",result.state)
     print("profundidad_solucion: ",len(result.path()))
